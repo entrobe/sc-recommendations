ce02f2050cd49aaa88d31f756c04adb8bc6a328b
Brian King <entrobe@gmail.com>
2015-02-25 11:47:48 -0500

    add some notes and a unit test, unit test not working yet

---
+++ src/test/java/com/bkmahout/recommender/RecommenderTest.groovy
@@ -0,0 +1,9 @@
+package com.bkmahout.recommender
+/**
+ * Created by Thoughtworker on 2/24/15.
+ */
+class RecommenderTest extends GroovyTestCase {
+    void testMain() {
+        assert 2 + 2 == 5
+    }
+}
