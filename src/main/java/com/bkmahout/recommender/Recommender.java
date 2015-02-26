50a6db02d26305db70627fcedc221e55258907e5
Brian King <entrobe@gmail.com>
2015-02-26 14:29:05 -0600

    add a user recommendation

--- src/main/java/com/bkmahout/recommender/Recommender.java
+++ src/main/java/com/bkmahout/recommender/Recommender.java
@@ -2,10 +2,17 @@ package com.bkmahout.recommender;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -18,11 +25,29 @@ public class Recommender {
             MovieLookerUpper titleFinder = new MovieLookerUpper("data/movie-information.csv");
             titleFinder.CreateMovieList();
 
+
             System.out.println("======Item Recommendations=====");
 
             List<ItemRecommendation> recommendations = ItemRecommender.GetRecommendations(dm);
 
             printItemRecommendations(recommendations, titleFinder);
+
+            System.out.println("======User Recommendations=====");
+
+            UserSimilarity similarity = new PearsonCorrelationSimilarity(dm);
+
+            UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, dm);
+
+            UserBasedRecommender recommender = new GenericUserBasedRecommender(dm, neighborhood, similarity);
+
+            List<RecommendedItem> userRecommendations = recommender.recommend(100, 5);
+            userRecommendations.forEach(rec -> {
+                try {
+                    System.out.println(titleFinder.GetTitleFromId(rec.getItemID()) + "," + rec.getValue());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         } catch (IOException e) {
             System.out.println("Oops!");
             e.printStackTrace();
@@ -42,4 +67,3 @@ public class Recommender {
         }
     }
 }
-
