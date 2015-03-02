bd06b83939fd7ca146412a2085f8a793b83734d6
Alejandro Beiderman <alejandrobeiderman@abeiderman.local>
2015-03-02 16:16:26 -0500

    Alejandro - Empty out the Recommender main method so we can start from scratch.

--- src/main/java/com/bkmahout/recommender/Recommender.java
+++ src/main/java/com/bkmahout/recommender/Recommender.java
@@ -8,43 +8,5 @@ import java.io.IOException;
 
 public class Recommender {
     public static void main(String[] args) {
-        try {
-            DataModel dm = new FileDataModel(new File("data/movies.csv"));
-            MovieLookerUpper titleFinder = new MovieLookerUpper("data/movie-information.csv");
-            titleFinder.CreateMovieList();
-
-        } catch (IOException e) {
-            System.out.println("Oops!");
-            e.printStackTrace();
-        }
     }
-
-//    private static void printUserRecommendations(MovieLookerUpper titleFinder, List<RecommendedItem> userRecommendations) {
-//        userRecommendations.forEach(rec -> {
-//            try {
-//                System.out.println(titleFinder.GetTitleFromId(rec.getItemID()) + "," + rec.getValue());
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//        });
-//    }
-//
-//    public static void printItemRecommendations(List<ItemRecommendation> recommendations, MovieLookerUpper titleFinder) throws IOException {
-//        recommendations.forEach(recommendation -> {
-//            try {
-//                String title = titleFinder.GetTitleFromId(recommendation.ItemId);
-//                recommendation.Recommendation.forEach(item -> {
-//                    try {
-//                        String recommendedTitle = titleFinder.GetTitleFromId(item.getItemID());
-//                        System.out.println(title + "," + recommendedTitle + "," + item.getValue());
-//                    }
-//                    catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                });
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//    }
 }
